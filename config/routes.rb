1770d9ad57c79cf63e48bd9bedc11aae5bcc643d
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-03 12:21:03 +0100

    added mister cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  root to: "cocktails#index"
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
